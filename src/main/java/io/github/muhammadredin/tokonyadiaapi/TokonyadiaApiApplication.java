c99859458aedcc0780c7b9c07409fafc837064ae
M Redin Birezqic <muhammadredin.b@gmail.com>
2024-10-03 13:33:49 +0700

    initial commit

---
+++ src/main/java/io/github/muhammadredin/tokonyadiaapi/TokonyadiaApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.muhammadredin.tokonyadiaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokonyadiaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TokonyadiaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/muhammadredin/tokonyadiaapi/TokonyadiaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.muhammadredin.tokonyadiaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokonyadiaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
